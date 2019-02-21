918982d90ad34a54c5ae89adaeef8ef7ac27ec20
Vinay <716678@cognizant.com>
2019-02-21 16:04:06 +0530

    Featured section updated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Grid, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import Featured from "../HeroContentBlock";
-import f1 from "./images/f1.jpeg";
 import "./index.css";
 
 const styles = theme => ({
@@ -26,6 +25,45 @@ const styles = theme => ({
   }
 });
 
+const featuredProducts = [
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/electronics/0000046_beats-pill-20-wireless-speaker_550.jpeg",
+    price: 39,
+    name: "portable-sound-speakers"
+  },
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/electronics/0000039_leica-t-mirrorless-digital-camera_550.jpeg",
+    price: 67,
+    name: "Leica T Mirrorless Digital camera"
+  },
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/toys/71B-0V7CutL._SX679_.jpg",
+    price: 90,
+    name: "wifi FPV Drone"
+  },
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/appreals/0000051_nike-floral-roshe-customized-running-shoes_550.jpg",
+    price: 99,
+    name: "Nike Shoes"
+  },
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/appreals/0000064_ray-ban-aviator-sunglasses_550.jpg",
+    price: 16,
+    name: "Sun glasses"
+  },
+  {
+    imageURL:
+      "https://storage.googleapis.com/gcpimage/images/toys/911c0QWW3uL._SL1500_.jpg",
+    price: 34,
+    name: "Basket Ball"
+  }
+];
+
 const HomeComponent = props => {
   const { classes } = props;
   return (
@@ -37,52 +75,31 @@ const HomeComponent = props => {
       </Grid>
       <h3 className="featuredProductsHeading">Featured products</h3>
       <Grid container className={classes.FeaturedProducts} spacing={8}>
-        <Grid
-          lg={2}
-          md={2}
-          sm={3}
-          xs={3}
-          item
-          className={classes.itemContainer}
-        >
-          <div className="imagewrapper">
-            <img src={f1} alt="featured-01" />
-            <div className="featuredProductButton">
-              <Button variant="outlined" className={classes.button}>
-                view
-              </Button>
-            </div>
-            <section className="featuredProductContent">
-              <div className="featuredContent">
-                <div>Apple iCam</div>
-                <div>$1300.00</div>
+        {featuredProducts.map(item => (
+          <Grid
+            lg={2}
+            md={2}
+            sm={3}
+            xs={3}
+            item
+            className={classes.itemContainer}
+          >
+            <div className="imagewrapper">
+              <img src={item.imageURL} alt="featured-01" />
+              <div className="featuredProductButton">
+                <Button variant="outlined" className={classes.button}>
+                  view
+                </Button>
               </div>
-            </section>
-          </div>
-        </Grid>
-        <Grid
-          lg={2}
-          md={2}
-          sm={3}
-          xs={3}
-          item
-          className={classes.itemContainer}
-        >
-          <div className="imagewrapper">
-            <img src={f1} alt="featured-01" />
-            <div className="featuredProductButton">
-              <Button variant="outlined" className={classes.button}>
-                view
-              </Button>
+              <section className="featuredProductContent">
+                <div className="featuredContent">
+                  <div className="featuredContent_name">{item.name}</div>
+                  <div>${item.price}</div>
+                </div>
+              </section>
             </div>
-            <section className="featuredProductContent">
-              <div className="featuredContent">
-                <div>Apple iCam</div>
-                <div>$1300.00</div>
-              </div>
-            </section>
-          </div>
-        </Grid>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
