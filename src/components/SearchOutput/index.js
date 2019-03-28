e93e39e0a990b6671b0add3a96374aa1c9efd730
Thomas <463599@cognizant.com>
2019-03-28 15:18:28 +0530

    camera changes

--- src/components/SearchOutput/index.js
+++ src/components/SearchOutput/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default ({ data }) => {
-    if(!data) return <div> No data found</div>;
-	if (data && data.length === 0) return <div>Loading......</div>;
-	return <div>{data.map((item) => <li>{item.name}</li>)}</div>;
+  if (!data) return <div style={{ marginTop: "150px" }}> No data found</div>;
+  if (data && data.length === 0)
+    return <div style={{ marginTop: "150px" }}>Loading......</div>;
+  return (
+    <div style={{ marginTop: "150px" }}>
+      {data.map(item => (
+        <li>{item.name}</li>
+      ))}
+    </div>
+  );
 };
