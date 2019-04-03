56a8f9b251dd234190cb8bf6752d5a2c58f36a81
ctscoderepo <anujgupta1403@gmail.com>
2019-04-03 10:53:26 +0530

    Changed https url

--- src/actions/Aioutput.js
+++ src/actions/Aioutput.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 export default encodedImageSrc => async dispatch => {
-	const imgSrc = encodedImageSrc.replace("data:image/png;base64,", "");
-	try {
-		const res = await axios.post(
-			"http://35.222.15.56/visionapi/searchImage",
-			{
-				requests: [
-					{
-						image: {
-							content: imgSrc
-						},
-						features: [
-							{
-								type: "LABEL_DETECTION"
-							}
-						]
-					}
-				]
-			}
-		);
-		dispatch({
-			type: "SEARCHRESULTSAI",
-			payload: res.data.products.products
-		});
-		return Promise.resolve(res);
-	} catch (err) {
-		return Promise.resolve();
-	}
+  const imgSrc = encodedImageSrc.replace("data:image/png;base64,", "");
+  try {
+    const res = await axios.post(
+      "https://35.227.204.18/visionapi/searchImage",
+      {
+        requests: [
+          {
+            image: {
+              content: imgSrc
+            },
+            features: [
+              {
+                type: "LABEL_DETECTION"
+              }
+            ]
+          }
+        ]
+      }
+    );
+    dispatch({
+      type: "SEARCHRESULTSAI",
+      payload: res.data.products.products
+    });
+    return Promise.resolve(res);
+  } catch (err) {
+    return Promise.resolve();
+  }
 };
